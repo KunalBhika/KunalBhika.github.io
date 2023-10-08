80c897aefb557f6ba9dc786eacbe30371d0e4c6d
KunalBhika <kunalbhika2@gmail.com>
2023-10-08 09:52:29 +0530

    Made a part of testimonial slider

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// About Section
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -13,4 +14,24 @@ const opentab = (tabname) => {
     }
     a.style.display = "block";
     targetLink.classList.add("active-link");
+}
+
+// Testimonials Section
+var slider_img = document.querySelector(".slider-img");
+var images = ['./images/kunal.jpg' , './images/kunal-about.jpg'];
+var reviewCount = document.querySelector(".large-span");
+var i = 0;   // Current image
+
+const nextReview = () => {
+    if(i == images.length-1)
+        i = -1;
+    slider_img.setAttribute('src' , images[++i]);
+    reviewCount.innerHTML = `0${i+1}`;
+}
+
+const prevReview = () => {
+    if(i == 0)
+        i = images.length;
+    slider_img.setAttribute('src' , images[--i]);
+    reviewCount.innerHTML = `0${i+1}`;
 }
