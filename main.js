b33be72b7e1bf23586824c4bc2c63ec5b8001b38
KunalBhika <kunalbhika2@gmail.com>
2025-01-20 23:07:58 +0530

    updated website

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const opentab = (tabname) => {
     const a = document.getElementById(tabname);
     const targetLink = event.target;
     console.log(tabname);
-    for(tablink of tablinks) {
+    for (tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents) {
+    for (tabcontent of tabcontents) {
         tabcontent.style.display = "none";
     }
     a.style.display = "block";
@@ -23,51 +23,51 @@ var reviewCount = document.querySelector(".large-span");
 var names = document.querySelector(".name-title");
 var designations = document.querySelector(".designation");
 
-var images = ['./images/isha.png' , './images/saloni.png' , './images/hiten.png' , './images/kanshika.png' , './images/ansh.png'];
+var images = ['./images/isha.png', './images/saloni.png', './images/hiten.png', './images/kanshika.png', './images/ansh.png'];
 var testiParas = [
-    "Kunal's dedication and perseverance are remarkable, serving as a shining example to everyone around him. Kunal approaches every task with unwavering determination and commitment, demonstrating a strong work ethic that is truly commendable. His resilience in the face of challenges is admirable, and his ability to stay focused on his goals is truly inspiring. His hard work will not only bring him success but will also motivate others to strive for excellence. He is a testament to the power of diligence and determination, and I feel fortunate to have such a dedicated friend in my life." ,
+    "Kunal's dedication and perseverance are remarkable, serving as a shining example to everyone around him. Kunal approaches every task with unwavering determination and commitment, demonstrating a strong work ethic that is truly commendable. His resilience in the face of challenges is admirable, and his ability to stay focused on his goals is truly inspiring. His hard work will not only bring him success but will also motivate others to strive for excellence. He is a testament to the power of diligence and determination, and I feel fortunate to have such a dedicated friend in my life.",
 
-    "Kunal Bhika stands as a shining example of talent and excellence. His relentless pursuit of perfection in every task he undertakes, coupled with his unwavering commitment to maintaining impeccable quality, is truly commendable. Whether it's his exceptional photography skills or his extensive knowledge of softwares, Kunal's capabilities are bound to inspire awe, <br>Working alongside Kunal on a team is not only a pleasure but also an enriching experience. His dedication, attention to detail, and ability to consistently deliver outstanding results contribute significantly to the success of any project. Kunal Bhika is undoubtedly a valuable asset in any collaborative endeavor." , 
+    "Kunal Bhika stands as a shining example of talent and excellence. His relentless pursuit of perfection in every task he undertakes, coupled with his unwavering commitment to maintaining impeccable quality, is truly commendable. Whether it's his exceptional photography skills or his extensive knowledge of softwares, Kunal's capabilities are bound to inspire awe, <br>Working alongside Kunal on a team is not only a pleasure but also an enriching experience. His dedication, attention to detail, and ability to consistently deliver outstanding results contribute significantly to the success of any project. Kunal Bhika is undoubtedly a valuable asset in any collaborative endeavor.",
 
-    "So what to say about you the best man i have ever met in InternWare, My team mentor and now more than my personal mentor, I have literally learned a lot of things from you and have seen such a dedicated person like you, I know that you are ready to help each one of us and that quality is the one i take forward so stay same as you are stay humble i wish you the best for your future. May god bless you always may you make each one of us and your parents proud. Love you!!" ,
+    "So what to say about you the best man i have ever met in InternWare, My team mentor and now more than my personal mentor, I have literally learned a lot of things from you and have seen such a dedicated person like you, I know that you are ready to help each one of us and that quality is the one i take forward so stay same as you are stay humble i wish you the best for your future. May god bless you always may you make each one of us and your parents proud. Love you!!",
 
-    "He is one of the most humble and funny people I know. But behind that sarcasm and a heartwarming personality is also an intellectual side that is extremely professional and that is his USP. A tech geek and a master of all, always helpful and kind. Thank you for all the memories we have made, it has been a pleasure working with you." ,
+    "He is one of the most humble and funny people I know. But behind that sarcasm and a heartwarming personality is also an intellectual side that is extremely professional and that is his USP. A tech geek and a master of all, always helpful and kind. Thank you for all the memories we have made, it has been a pleasure working with you.",
 
     "I had the privilege of working with Kunal bhaiya, and I am immensely grateful for the guidance and support he provided as my technical mentor. He truly embodies the qualities of an exceptional mentor, making my learning experience incredibly valuable and enriching."
 ]
-var testiNames = ["Isha" , "Saloni Indra" , "Hiten Anand" , "Kanshika Nagpal" , "Ansh Sudan"];
-var testiDesig = ["Business Development Mentor @ InternWare" , "Chief Financial Officer @ InternWare" , "Technical Executive @ InternWare" , "Chief Corporate Relations @ InternWare" , "Technical Executive @ InternWare"];
+var testiNames = ["Isha", "Saloni Indra", "Hiten Anand", "Kanshika Nagpal", "Ansh Sudan"];
+var testiDesig = ["Business Development Mentor @ InternWare", "Chief Financial Officer @ InternWare", "Technical Executive @ InternWare", "Chief Corporate Relations @ InternWare", "Technical Executive @ InternWare"];
 
 var i = 0;   // Current image
 
 // initail testimonial
-slider_img.setAttribute('src' , images[i]);
+slider_img.setAttribute('src', images[i]);
 review.innerHTML = `${testiParas[i]}`;
 names.innerHTML = `${testiNames[i]}`;
-reviewCount.innerHTML = `0${i+1}`;
+reviewCount.innerHTML = `0${i + 1}`;
 designations.innerHTML = `${testiDesig[i]}`;
 
 const nextReview = () => {
-    if(i == images.length-1)
+    if (i == images.length - 1)
         i = -1;
-    
+
     i++;
-    slider_img.setAttribute('src' , images[i]);
+    slider_img.setAttribute('src', images[i]);
     review.innerHTML = `${testiParas[i]}`;
     names.innerHTML = `${testiNames[i]}`;
-    reviewCount.innerHTML = `0${i+1}`;
+    reviewCount.innerHTML = `0${i + 1}`;
     designations.innerHTML = `${testiDesig[i]}`;
 }
 
 const prevReview = () => {
-    if(i == 0)
+    if (i == 0)
         i = images.length;
 
     --i;
-    slider_img.setAttribute('src' , images[i]);
+    slider_img.setAttribute('src', images[i]);
     review.innerHTML = `${testiParas[i]}`;
     names.innerHTML = `${testiNames[i]}`;
-    reviewCount.innerHTML = `0${i+1}`;
+    reviewCount.innerHTML = `0${i + 1}`;
     designations.innerHTML = `${testiDesig[i]}`;
 }
 
@@ -84,25 +84,25 @@ const closeMenu = () => {
 
 // Contact form script
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz0xr4B3xKuQqJPhmAQfQXd-qdfkzPRksF93bbicGpfbhZEMijJRh1t_HRXlXvCKjY/exec'
-  const form = document.forms['submit-to-google-sheet'];
-  const msg = document.getElementById('msg');
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById('msg');
 
-  form.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-          msg.innerHTML = `Message sent successfully <i class="fa-solid fa-circle-check"></i>`;
-          setTimeout(() => {
-              msg.innerHTML = "";
-          } , 5000);
-          form.reset();
-      })
-      .catch(error => {
-          msg.style.color = '#c5163d';
-          msg.innerHTML = `Some error occurred. Try Again after sometime <i class="fa-solid fa-circle-xmark"></i>`;
-          setTimeout(() => {
-              msg.innerHTML = "";
-         } , 5000);
-         form.reset();
-      })
-  })
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = `Message sent successfully <i class="fa-solid fa-circle-check"></i>`;
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => {
+            msg.style.color = '#c5163d';
+            msg.innerHTML = `Some error occurred. Try Again after sometime <i class="fa-solid fa-circle-xmark"></i>`;
+            setTimeout(() => {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+})
